e7ec0846d4640e97c3d3305f428d8bf14a0bcd15
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2022-11-07 21:34:03 +0100

    Added Image to Mat function

--- ComputerVision.cs
+++ ComputerVision.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV;
+using Emgu.CV.Structure;
 using System.Drawing;
 
 namespace Spirit_Studio
@@ -14,5 +15,33 @@ namespace Spirit_Studio
         {
             return new Bitmap(imgToResize, size);
         }
+
+        public static Mat GetMatFromSDImage(System.Drawing.Image image)
+        {
+            int stride;
+            Bitmap bmp = new Bitmap(image);
+
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            System.Drawing.Imaging.PixelFormat pf = bmp.PixelFormat;
+            if (pf == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+            {
+                stride = bmp.Width * 4;
+            }
+            else
+            {
+                stride = bmp.Width * 3;
+            }
+
+            Image<Bgra, byte> cvImage = new Image<Bgra, byte>(bmp.Width, bmp.Height, stride, (IntPtr)bmpData.Scan0);
+
+            bmp.UnlockBits(bmpData);
+
+            return cvImage.Mat;
+        }
     }
+
+
 }
+
