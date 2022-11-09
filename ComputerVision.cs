00a6ee1987adcbe16877992381be22e2894f7625
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2022-11-10 00:15:52 +0100

    Saving image to AppData folder

--- ComputerVision.cs
+++ ComputerVision.cs
@@ -7,6 +7,7 @@ using Emgu.CV;
 using Emgu.CV.Structure;
 using System.Drawing;
 using System.Diagnostics;
+using System.IO;
 
 namespace Spirit_Studio
 {
@@ -21,32 +22,6 @@ namespace Spirit_Studio
                 Debug.WriteLine("size is NULL");
 
             return new Bitmap(imgToResize, size);
-        }
-
-        //Stolen from https://stackoverflow.com/questions/40384487/system-drawing-image-to-emgu-cv-mat
-        public static Mat GetMatFromSDImage(Image image)
-        {
-            int stride;
-            Bitmap bmp = new Bitmap(image);
-
-            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
-            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-
-            System.Drawing.Imaging.PixelFormat pf = bmp.PixelFormat;
-            if (pf == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
-            {
-                stride = bmp.Width * 4;
-            }
-            else
-            {
-                stride = bmp.Width * 3;
-            }
-
-            Image<Bgra, byte> cvImage = new Image<Bgra, byte>(bmp.Width, bmp.Height, stride, (IntPtr)bmpData.Scan0);
-
-            bmp.UnlockBits(bmpData);
-
-            return cvImage.Mat;
         }
 
         public static Bitmap GetAbsDifference(Image reference, Image newImage, out double? differencePercentage)
@@ -74,6 +49,10 @@ namespace Spirit_Studio
             differencePercentage = percentageChanged;
 
             Debug.WriteLine($"percentage of pixels that changed: {percentageChanged}");
+
+            if (differencePercentage > 10)
+                newImage.Save(Path.Combine("C:/ProgramData/Spirit Lab/PhotoShoot", $"{DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")}.bmp"), System.Drawing.Imaging.ImageFormat.Bmp);
+            
 
             return thresholded.ToBitmap();
         }
